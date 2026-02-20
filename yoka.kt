2d953f49c021a929b9089c5a946a1fa1ba7c6252
Achmad Baihaqi <138300409+Poco212@users.noreply.github.com>
2026-02-21 01:03:37 +0700

    update

--- yoka.kt
+++ yoka.kt
@@ -3,7 +3,6 @@ fun main() {
     val harpok = 27000
     val harjul = 60000
     var diskon = 0.10 
-    var total_diskon = harjul * diskon
-    var total = harjul - total_diskon
+    var total = harjul * diskon
     println (total)
 }
