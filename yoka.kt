d9b0bbe06afe2f55eb3b94ccbd1e052a6ec88545
Achmad Baihaqi <138300409+Poco212@users.noreply.github.com>
2026-02-21 02:27:49 +0700

    update

--- yoka.kt
+++ yoka.kt
@@ -4,7 +4,8 @@ fun main() {
     val harjul = 60000.0
     var diskon = 0.10 
     for (i in 1..7) {
-        println ("Hari ke $i = $harjul - ($diskon / 2)")
+        var penber = harjul - (diskon / 2)
+        println ("Hari ke $i = $penber")
     }
 
     
