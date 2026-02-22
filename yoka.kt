afaea23a5780b08c74eda692845ba2c28de384df
Achmad Baihaqi <138300409+Poco212@users.noreply.github.com>
2026-02-22 20:04:30 +0700

    update

--- yoka.kt
+++ yoka.kt
@@ -3,9 +3,11 @@ fun main() {
     val harpok = 27000.0
     val harjul = 60000.0
     var diskon = 0.10 
+    // misalnya
+    var beli = 5
     for (i in 1..7) {
-        var predis = harjul - (harjul * diskon)
-        println (predis)
+        val pajak = if ( i % 2 == 0) 0.20 else 0.125
+        println ("Hari ke $i = $pajak")
     }
     
 }
