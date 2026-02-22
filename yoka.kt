fb1b4c968a120a8c2f5fd9ac4c2b96250fe7d3b1
Achmad Baihaqi <138300409+Poco212@users.noreply.github.com>
2026-02-22 20:00:30 +0700

    update

--- yoka.kt
+++ yoka.kt
@@ -4,9 +4,8 @@ fun main() {
     val harjul = 60000.0
     var diskon = 0.10 
     for (i in 1..7) {
-        var predis = harjul - ${harjul * diskon}
+        var predis = harjul - (harjul * diskon)
         println (predis)
-        diskon /= 2
     }
     
 }
