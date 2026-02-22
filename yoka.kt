0a1ddcb005486376c481d444832bc4e26bbc848d
Achmad Baihaqi <138300409+Poco212@users.noreply.github.com>
2026-02-22 20:28:19 +0700

    update

--- yoka.kt
+++ yoka.kt
@@ -1,13 +1,26 @@
 
 fun main() {
+    
+    // harga pokok barang
     val harga_pokok = 27000.0
+    
+    // harga jual barang
     val harga_jualan = 60000.0
-    var diskon = 0.10 
-    // misalnya
+    
+    // diskon hari pertama 10% dan berkurang setengah tiap hari nya
+    var diskon = 0.10  
+    
+    // misalnya pembelian 5 dan bertambah 2 setiap harinya
     var beli = 5
     for (i in 1..7) {
+        // pajak
         val pajak = if ( i % 2 == 0) 0.20 else 0.125
-        println ("Hari ke $i = $pajak")
+        
+        //harga setelah diskon
+        val harjul_disk = harga_jualan - (harga_jualan * diskon)
+        
+        println ("Hari ke $i = $harjul_disk")
+        diskon /= 2
     }
     
 }
