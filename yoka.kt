4439b3e3caa80fdc79eb3c5761c0897e2813fe9c
Achmad Baihaqi <138300409+Poco212@users.noreply.github.com>
2026-02-22 20:05:05 +0700

    update

--- yoka.kt
+++ yoka.kt
@@ -1,7 +1,7 @@
 
 fun main() {
-    val harpok = 27000.0
-    val harjul = 60000.0
+    val harga_pokok = 27000.0
+    val harga_jualan = 60000.0
     var diskon = 0.10 
     // misalnya
     var beli = 5
